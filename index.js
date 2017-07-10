9cad8cd4e84c3d1548deea67f1798b568455ec65
Jan Phillip Carreon <janphillipcarreon@yahoo.com>
2017-07-10 15:36:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 // Code your solution here!
+function printString(myString) {
+  console.log(myString[0]);
 
+  if (myString.length > 1) {
+    let mySubString = myString.substring(1, myString.length);
+    printString(mySubString);
+  } else {
+    return true;
+  }
+}
+
+
+
+function reverseString(string){
+  if(string.length === 1){
+    return string;
+  }else{
+    return string[string.length-1] + reverseString(string.substr(0,string.length-1));
+  }
+}
+
+
+function isPalindrome(word){
+  if(word.toLowerCase() === reverseString(word.toLowerCase())){
+
+    return true;
+  }else{
+    return false;
+  }
+}
+
+// console.log(recursivePalindrome('dkajdslfkjad'));
